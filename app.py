cbb876c0604c4dcc92911fabec7607a474d37021
Hugo <hafizhhugo@gmail.com>
2025-01-02 23:06:51 +0700

    remove matplotlib

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, classification_report
 from imblearn.over_sampling import SMOTE
 import joblib  
