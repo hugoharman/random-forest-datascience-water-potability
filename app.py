028d407ea35236ce21bf07e3ff90602ca06d4f59
Hugo <hafizhhugo@gmail.com>
2025-01-02 23:08:18 +0700

    requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import classification_report
 from imblearn.over_sampling import SMOTE
 import joblib  
 import os
